71e79d61ac3a591f9350e526c0cf0dfdd252021b
Facundo A. Pestana <136341998+FacuPestana@users.noreply.github.com>
2023-11-18 19:48:54 -0300

    1.0 | Final Version

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function(){
+    document.getElementById('viewAlerta').addEventListener('click',function(){
+        alert('Feliz Cumple Papiii!!!! Te quiero mucho y te deseo un muy buen feliz cumpleaños.')
+        prompt('Te gusto?')
+        let saludo1
+        if (saludo1 = 'si' || 'Si') {
+             alert('Gracias Papi, te Quiero un monton')
+        }
+       
+    })
+ 
+}) 
